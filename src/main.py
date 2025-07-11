63bc5a029ccdbd68789dc61e07fb8eaba03395a1
ryanrugh <ryanrugh@gmail.com>
2025-07-11 12:42:57 -0400

    modified basepath logic in main.py to make it clearer

--- src/main.py
+++ src/main.py
@@ -8,28 +8,24 @@ dir_path_static = root_dir_path + "/static"
 dir_path_docs = root_dir_path + "/docs"
 dir_path_content = root_dir_path + "/content"
 dir_path_template = root_dir_path + "/template.html"
-
-if len(sys.argv) > 1:
-    basepath = sys.argv[1]
-else:
-    basepath = "/"
+default_basepath = "/"
 
 def main():
     """
-    Main function to generate the static site.
+    Main entry point for the static site generator.
 
-    This function performs the following steps:
-    1. Deletes the existing 'docs' directory if it exists.
-    2. Copies static files from the static directory to the 'docs' directory.
-    3. Generates HTML files from markdown content using the provided templates.
+    - Determines the base path from command-line arguments or uses the default.
+    - Deletes the existing 'docs' directory if it exists.
+    - Copies static files from the static directory to the 'docs' directory.
+    - Generates HTML pages from markdown content using the specified template.
 
-    Assumes the following variables are defined in the global scope:
-    - dir_path_docs: Path to the output 'docs' directory.
-    - dir_path_static: Path to the static files directory.
-    - dir_path_content: Path to the markdown content directory.
-    - dir_path_template: Path to the HTML templates directory.
-    - basepath: Base path for the generated site.
+    Raises:
+        Any exceptions raised by file operations or page generation functions.
     """
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     print("Deleting docs directory...")
     if os.path.exists(dir_path_docs):
         shutil.rmtree(dir_path_docs)
